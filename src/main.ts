d96bf81a56b65345d41453e43bd5d35774bb988c
venomissionary <stevehernandes53@gmail.com>
2024-10-26 12:13:22 -0700

    - step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "Steven's game try";
+const APP_NAME = "Steven's game";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const canvas = document.createElement("canvas");
+canvas.height = 256; 
+canvas.width = 256;
+
+const ctx = canvas.getContext("2d");
+
+if (ctx) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 256, 256); 
+} 
+
+app.appendChild(canvas);
+
+
