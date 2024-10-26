4106afee26433ebe515246524ca542c0732ab10c
venomissionary <stevehernandes53@gmail.com>
2024-10-26 11:20:29 -0700

    change game name 2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Steven's game";
+const APP_NAME = "Steven's game try";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
